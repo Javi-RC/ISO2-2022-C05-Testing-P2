880441b22b48d46469596d99e06777a951b3fa32
kik548 <villarceakike@gmail.com>
2022-12-11 13:49:25 +0100

    triangulo 0.1.0 añadidos atributos y funciones basicas

--- Ejercicio2/src/main/java/ISO2_C05/Ejercicio2/Triangulo.java
+++ Ejercicio2/src/main/java/ISO2_C05/Ejercicio2/Triangulo.java
@@ -2,4 +2,69 @@ package ISO2_C05.Ejercicio2;
 
 public class Triangulo {
 
+	private double a;
+	private double b;
+	private double c;
+	private double alpha;
+	private double beta;
+	private double delta;
+	
+	public Triangulo(double a, double b, double c, double alpha, double beta, double delta) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		this.alpha = alpha;
+		this.beta = beta;
+		this.delta = delta;
+	}
+
+	public double getA() {
+		return a;
+	}
+
+	public void setA(double a) {
+		this.a = a;
+	}
+
+	public double getB() {
+		return b;
+	}
+
+	public void setB(double b) {
+		this.b = b;
+	}
+
+	public double getC() {
+		return c;
+	}
+
+	public void setC(double c) {
+		this.c = c;
+	}
+
+	public double getAlpha() {
+		return alpha;
+	}
+
+	public void setAlpha(double alpha) {
+		this.alpha = alpha;
+	}
+
+	public double getBeta() {
+		return beta;
+	}
+
+	public void setBeta(double beta) {
+		this.beta = beta;
+	}
+
+	public double getDelta() {
+		return delta;
+	}
+
+	public void setDelta(double delta) {
+		this.delta = delta;
+	}
+
+
 }
