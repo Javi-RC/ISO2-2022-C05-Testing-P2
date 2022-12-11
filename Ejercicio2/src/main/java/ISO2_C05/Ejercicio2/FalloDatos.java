9aefb61ded2d52bfc5f6084fbc779cd2a9a3c4fd
kik548 <villarceakike@gmail.com>
2022-12-11 14:21:38 +0100

    Añadida la excepción FalloDatos 1.0.0 Añadida funcionalidad a triangulo v 1.0.0

---
+++ Ejercicio2/src/main/java/ISO2_C05/Ejercicio2/FalloDatos.java
@@ -0,0 +1,9 @@
+package ISO2_C05.Ejercicio2;
+
+public class FalloDatos extends Exception {
+	
+	
+	public FalloDatos(String cadena) {
+		super(cadena);
+	}
+}
--- Ejercicio2/src/main/java/ISO2_C05/Ejercicio2/Triangulo.java
+++ Ejercicio2/src/main/java/ISO2_C05/Ejercicio2/Triangulo.java
@@ -65,6 +65,62 @@ public class Triangulo {
 	public void setDelta(double delta) {
 		this.delta = delta;
 	}
+	private String getTipoLadosTriangulo(Triangulo triangulo) {
+		
+		String tipo="";
+		if (triangulo.getA()==triangulo.getB() && triangulo.getB() == triangulo.getC())
+			tipo="equilatero";
+		else if(triangulo.getB()==triangulo.getC()){
+			tipo="isosceles";
+			}
+		else {
+			tipo="escaleno";
+			
+		}
+
+		return tipo;
+	}
+	
+	private String getTipoAnguloTriangulo(Triangulo triangulo)throws FalloDatos{
+		
+		String tipo = "";
+		double suma = triangulo.getAlpha()+ triangulo.getBeta()+triangulo.getDelta();
+		if(suma!=180) {
+			throw new FalloDatos("La suma de los angulos debe de ser 180");
+		}
+		if (triangulo.getAlpha()==90) {
+			tipo = "rectangulo";
+		}
+		else if(triangulo.getAlpha()>90) {
+			tipo="obtusangulo";
+		}
+		else{
+			
+			tipo="acutangulo";
+		}
+		return tipo;
+	}
+	
+	public String getTipoTriangulo(Triangulo triangulo,boolean decision) throws FalloDatos {
+		String tipo="";
+		
+		if (triangulo.getA()<=0 || triangulo.getB() <=0 ||
+			triangulo.getC() <=0 || triangulo.getAlpha()<=0 ||
+			triangulo.getBeta()<=0 || triangulo.getDelta()<=0) {
+			throw new FalloDatos("Los lados y angulos deben ser mayor que 0");
+		}
+			
+			
+		if (decision) {
+			tipo=getTipoLadosTriangulo(triangulo);
+		}
+		else {
+			
+			tipo=getTipoAnguloTriangulo(triangulo);
+		}
+
+		return tipo;
+	}
 
 
 }
