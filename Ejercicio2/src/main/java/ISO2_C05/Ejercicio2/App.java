ef748db93a3516a0fabf40b64e0de2cf8e4885a3
kik548 <villarceakike@gmail.com>
2022-12-11 13:41:34 +0100

    Cambios a la estructura

---
+++ Ejercicio2/src/main/java/ISO2_C05/Ejercicio2/App.java
@@ -0,0 +1,13 @@
+package ISO2_C05.Ejercicio2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
