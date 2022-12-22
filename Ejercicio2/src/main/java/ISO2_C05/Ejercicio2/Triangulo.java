45a56e0b2bf6fd3ecd2b542f79cbaa0ab6aae6fe
kik548 <villarceakike@gmail.com>
2022-12-22 15:51:10 +0100

    Eliminación de funciones y variables no utilizadas

--- Ejercicio2/src/main/java/ISO2_C05/Ejercicio2/Triangulo.java
+++ Ejercicio2/src/main/java/ISO2_C05/Ejercicio2/Triangulo.java
@@ -1,70 +1,55 @@
 package ISO2_C05.Ejercicio2;
 
+
+
 public class Triangulo {
 
-	private double a; // Base
+	private double ladoA;
 	private double b; 
-	private double c; // Hipotenusa
+	private double ladoC;
 	private double alpha;
 	private double beta;
 	private double delta;
 	
-	public Triangulo(double a, double b, double c, double alpha, double beta, double delta) {
-		this.a = a;
-		this.b = b;
-		this.c = c;
+
+	public Triangulo(double ladoA, double ladoB, double ladoC, double alpha, double beta, double delta) {
+		this.ladoA = ladoA;
+		this.b = ladoB;
+		this.ladoC = ladoC;
 		this.alpha = alpha;
 		this.beta = beta;
 		this.delta = delta;
 	}
 
 	public double getA() {
-		return a;
+		return ladoA;
 	}
 
-	public void setA(double a) {
-		this.a = a;
-	}
 
 	public double getB() {
 		return b;
 	}
 
-	public void setB(double b) {
-		this.b = b;
-	}
 
 	public double getC() {
-		return c;
+		return ladoC;
 	}
 
-	public void setC(double c) {
-		this.c = c;
-	}
 
 	public double getAlpha() {
 		return alpha;
 	}
 
-	public void setAlpha(double alpha) {
-		this.alpha = alpha;
-	}
 
 	public double getBeta() {
 		return beta;
 	}
 
-	public void setBeta(double beta) {
-		this.beta = beta;
-	}
 
 	public double getDelta() {
 		return delta;
 	}
 
-	public void setDelta(double delta) {
-		this.delta = delta;
-	}
 	private String getTipoLadosTriangulo(Triangulo triangulo) {
 		
 		String tipo="";
