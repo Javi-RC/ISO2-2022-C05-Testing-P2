1a1940dce0aeb3fcd6a47004e69e4aeb00f3b4aa
kik548 <villarceakike@gmail.com>
2022-12-22 16:08:00 +0100

    Añadido JavaDoc

--- Ejercicio2/src/main/java/ISO2_C05/Ejercicio2/FalloDatos.java
+++ Ejercicio2/src/main/java/ISO2_C05/Ejercicio2/FalloDatos.java
@@ -2,7 +2,11 @@ package ISO2_C05.Ejercicio2;
 
 public class FalloDatos extends Exception {
 	
-	
+	/**
+	 * Excepcion creada para utilizarse ante fallos de datos  
+	 * numeros negativos o 0 y  angulos no congruentes
+	 * @param cadena Contenido de la excepcion
+	 */
 	public FalloDatos(String cadena) {
 		super(cadena);
 	}
--- Ejercicio2/src/main/java/ISO2_C05/Ejercicio2/Triangulo.java
+++ Ejercicio2/src/main/java/ISO2_C05/Ejercicio2/Triangulo.java
@@ -1,7 +1,12 @@
 package ISO2_C05.Ejercicio2;
 
-
-
+/**
+ * 
+ * @author Javier Rodriguez Castellano
+ * @author Enrique Javier Villar Cea
+ *
+ *
+ */
 public class Triangulo {
 
 	private double ladoA;
@@ -11,7 +16,15 @@ public class Triangulo {
 	private double beta;
 	private double delta;
 	
-
+/**
+ * 
+ * @param ladoA lado 1 de un triangulo
+ * @param ladoB lado 2 de un triangulo
+ * @param ladoC lado 3 de un triangulo, en un triangulo rectangulo sería la hipotenusa
+ * @param alpha angulo entre ladoA y ladoB
+ * @param beta angulo entre ladoA y ladoC
+ * @param delta angulo entre ladoB y ladoC
+ */
 	public Triangulo(double ladoA, double ladoB, double ladoC, double alpha, double beta, double delta) {
 		this.ladoA = ladoA;
 		this.b = ladoB;
@@ -20,36 +33,60 @@ public class Triangulo {
 		this.beta = beta;
 		this.delta = delta;
 	}
-
+/**
+ * 
+ * @return tamaño del lado 1 de un triangulo
+ */
 	public double getA() {
 		return ladoA;
 	}
 
-
+/**
+ * 
+ * @return tamaño del lado 2 de un triangulo
+ */
 	public double getB() {
 		return b;
 	}
 
-
+/**
+ * 
+ * @return tamaño del lado 3 de un triangulo
+ *  */
 	public double getC() {
 		return ladoC;
 	}
 
-
+/**
+ * 
+ * @return angulo entre ladoA y ladoB
+ */
 	public double getAlpha() {
 		return alpha;
 	}
 
-
+/**
+ * 
+ * @return angulo entre ladoA y ladoC
+ */
 	public double getBeta() {
 		return beta;
 	}
 
-
+/**
+ * 
+ * @return angulo entre ladoB y ladoC
+ */
 	public double getDelta() {
 		return delta;
 	}
 
+/**
+ * 
+ * Función que devuelve el tipo de triangulo dependiendo de sus lados
+ * @param triangulo El triangulo del que se quiere conecer su tipo
+ * @return El tipo de triangulo: equilatero, isosceles o escaleno
+ */
 	private String getTipoLadosTriangulo(Triangulo triangulo) {
 		
 		String tipo="";
@@ -65,7 +102,12 @@ public class Triangulo {
 
 		return tipo;
 	}
-	
+/**
+ * Función que devuelve el tipo de triangulo dependiendo de sus angulos
+ * @param triangulo El triangulo del que se quiere conecer su tipo
+ * @return El tipo de triangulo: rectangulo, obtusangulo, acutangulo
+ * @throws FalloDatos, cuando la suma de los angulos del triangulo no son iguales a 180
+ */
 	private String getTipoAnguloTriangulo(Triangulo triangulo)throws FalloDatos{
 		
 		String tipo = "";
@@ -85,7 +127,13 @@ public class Triangulo {
 		}
 		return tipo;
 	}
-	
+/**
+ * Funcion para obtener el tipo de triangulo
+ * @param triangulo El triangulo del que se quiere conocer su tipo
+ * @param decision obtener el tipo de triangulo. True: segun los lados, False: segun sus angulos
+ * @return tipo El tipo de triangulo
+ * @throws FalloDatos Lanzada cuando algun parametro del triangulo es menor o igual a 0
+ */
 	public String getTipoTriangulo(Triangulo triangulo,boolean decision) throws FalloDatos {
 		String tipo="";
 		
